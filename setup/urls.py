de8ec87dbcbb0e028462ee845e76aff42ff08f40
Omega050 <mauro-jr50@hotmail.com>
2024-06-14 15:41:27 -0300

    AAA

--- setup/urls.py
+++ setup/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
@@ -21,4 +23,4 @@ from django.contrib import admin
 urlpatterns= [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
